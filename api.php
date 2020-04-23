cc5e25a164317cc75273de776aa9da2c51029258
Huynguyen1521999 <44427591+Huynguyen1521999@users.noreply.github.com>
2020-04-23 10:56:27 +0700

    Update api.php

--- api.php
+++ api.php
@@ -59,14 +59,28 @@ class api extends restful_api {
         }
     }
 
-    function getday($params)
+     function getyear($params)
     {
-        $date = "22-4-2020"
-        $myDate = new DateTime($date);
-        $myDate->modify("tomorrow");
-        echo $myDate->format("d, m, Y");
-        
-    }
+        if(empty($params[0])||!empty($params[1]))
+        {
+            $data = array("status" => false, "data" => array());
+            return $data;
+        }elseif((double)$params[0]-(int)$params[0]!=0||!(int)$params[0]>0)
+        {
+            $data = array("status" => false, "data" => array());
+            return $data;
+        }
+        else
+        {
+            $x = (int)$params[0];
+            if ($x % 400 == 0 || $x % 4 == 0 && $x % 100 != 0) {
+                $data = array("status" => true, "data" => array("result" => "Nam " . $x . " la nam nhuan"));
+
+            } else {
+                $data = array("status" => true, "data" => array("result" => "Nam " . $x . " khong phai la nam nhuan"));
+            }
+            return $data;
+        }
 }
 
 $user_api = new api();
