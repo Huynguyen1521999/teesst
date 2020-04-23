e1950ac11bf00e7755397802ad070074312b5169
Huynguyen1521999 <44427591+Huynguyen1521999@users.noreply.github.com>
2020-04-23 11:43:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <script>
-    
-   function getLinkJSONyear() {
+    function getLinkJSONtamgiac() {
+        var a,b,c;
+        a = document.getElementById("inputa").value;
+        b = document.getElementById("inputb").value;
+        c = document.getElementById("inputc").value;
+        window.open("api-tamgiac.php/checktamgiac/"+a+"/"+b+"/"+c);
+    }
+    function getLinkJSONngayketiep() {
         var a,b,c;
         a = document.getElementById("inputday").value;
         b = document.getElementById("inputmonth").value;
         c = document.getElementById("inputyear").value;
-        window.open("api.php/checkngayketiep/"+a+"/"+b+"/"+c);
+        window.open("api-ngayketiep.php/checkngayketiep/"+a+"/"+b+"/"+c);
     }
-
+    function getData()
+    {
+        document.getElementById("txt_apijsonpt2").innerHTML = window.location.host +"/ptb2-php/api-tamgiac.php/checktamgiac/a/b/c";
+        document.getElementById("txt_apijsonngayketiep").innerHTML = window.location.host +"/ptb2-php/api-ngayketiep.php/checkngayketiep/day/month/year";
+    }
 </script>
 <!DOCTYPE html>
 <html lang="vi" xmlns="http://www.w3.org/1999/xhtml">
@@ -18,16 +28,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-
-<h1>Ngày tiếp theo</h1>
+<h1>Kiem tra tam giac</h1>
+<a>a,b,c</a>
 <form>
-    <label for="fname">Ngày:</label><br>
-    <input type="number" id="inputday" name="fname"><br>
-    <label for="lname">Tháng:</label><br>
-    <input type="number" id="inputmonth" name="lname"><br>
-    <label for="lname">Năm:</label><br>
-    <input type="number" id="inputyear" name="lname"><br>
-    <input type ="button" name="OKE" value="Submit" onclick="getLinkJSONyear()">
+    <label for="fname">a</label><br>
+    <input type="number" id="inputa" name="fname"><br>
+    <label for="lname">b</label><br>
+    <input type="number" id="inputb" name="lname"><br>
+    <label for="lname">c</label><br>
+    <input type="number" id="inputc" name="lname"><br>
+    <input type ="button" name="OKE" value="Phân Loại" onclick="getLinkJSONtamgiac()">
 </form>
+<h3>Api json pt2: </h3><h4 id="txt_apijsonpt2"></h4>
+<h1>Kiểm tra ngày tiếp theo</h1>
+<form>
+    <label for="lname">Nhập ngày</label><br>
+    <input type="number" id="inputday" name="lname"><br>
+   <label for="lname">Nhập tháng</label><br>
+    <input type="number" id="inputmonth" name="lname"><br>
+   <label for="lname">Nhập năm</label><br>
+    <input type="number" id="inputyear" name="lname"><br>
+    <input type ="button" name="OKE" value="Ngày tiếp thep" onclick="getLinkJSONngayketiep()">
+</form>
+<h3>Api json pt2: </h3><h4 id="txt_apijsonngayketiep"></h4>
+<script>getData();</script>
 </body>
 </html>
