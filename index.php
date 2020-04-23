900a2a6ad785a170dbeab88a90ac96f6acb6eaff
Huynguyen1521999 <44427591+Huynguyen1521999@users.noreply.github.com>
2020-04-23 11:17:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <script>
-    function getLinkJSONtamgiac() {
-        var a,b,c;
-        a = document.getElementById("inputa").value;
-        b = document.getElementById("inputb").value;
-        c = document.getElementById("inputc").value;
-        window.open("api.php/checktamgiac/"+a+"/"+b+"/"+c);
-    }
+    
     function getLinkJSONyear() {
         var a,b,c;
         a = document.getElementById("inputday").value;
@@ -23,16 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
-<h1>Phân Loại Tam Giác</h1>
-<form>
-    <label for="fname">a</label><br>
-    <input type="number" id="inputa" name="fname"><br>
-    <label for="lname">b</label><br>
-    <input type="number" id="inputb" name="lname"><br>
-    <label for="lname">c</label><br>
-    <input type="number" id="inputc" name="lname"><br>
-    <input type ="button" name="OKE" value="Submit" onclick="getLinkJSONtamgiac()">
-</form>
+
 <h1>Nhập ngày tháng năm</h1>
 <form>
     <label for="fname">Ngày:</label><br>
