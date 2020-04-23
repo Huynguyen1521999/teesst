c3c8a466da1a2ed14d55bdfe34f16c8029a39a1e
Huynguyen1521999 <44427591+Huynguyen1521999@users.noreply.github.com>
2020-04-23 11:44:54 +0700

    Update api.php

--- api.php
+++ api.php
@@ -11,6 +11,13 @@ class api extends restful_api
         parent::__construct();
     }
 
+     function checktamgiac()
+    {
+        if($this->method == 'GET')
+        {
+            $this->response(200,$this->gettamgiac($this->params));
+        }
+    }
     function checkngayketiep()
     {
         if ($this->method == 'GET')
@@ -18,7 +25,41 @@ class api extends restful_api
             $this->response(200, $this->getngayketiep($this->params));
         }
     }
-
+  function gettamgiac($params)
+    {
+        if(empty($params[0])||empty($params[1])||empty($params[2])||!empty($params[3]))
+        {
+            return array("status" => false, "data" => array());
+        }
+        else{
+            $a=(double)$params[0];
+            $b=(double)$params[1];
+            $c=(double)$params[2];
+            if(($a + $b) > $c && ($b + $c) > $a && ($c + $a) > $b)
+            {
+               return $data="Day la tam giac thuong";
+            }
+            elseif($a == $b && $b == $c)
+            {
+                return $data = "Day la tam giac deu";
+            }
+            elseif($a == $b || $a == $c || $c == $b)
+            {
+                if($a * $a == $b * $b + $c * $c || $b * $b == $a * $a + $c * $c || $c * $c == $a * $a + $b *$b)
+                {
+                    return $data = "Day la tam giac vuong can";
+                }
+                else
+                {
+                    return $data = "Day la tam giac can";
+                }
+            }
+            elseif($a2==$b2+$c2 || $b2==$a2+$c2 || $c2==$a2+$b2)
+            {
+                return $data = "Day la tam giac vuong";
+            }
+        }
+    }
     function getngayketiep($params)
     {
         if(empty($params[0])||empty($params[1])||empty($params[2])||!empty($params[3]))
