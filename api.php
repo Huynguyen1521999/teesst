eea96f602661029c74bc1c665dfa3d8ee1f56f32
Huynguyen1521999 <44427591+Huynguyen1521999@users.noreply.github.com>
2020-04-23 10:36:04 +0700

    Update api.php

--- api.php
+++ api.php
@@ -59,28 +59,13 @@ class api extends restful_api {
         }
     }
 
-    function getyear($params)
+    function getday($params)
     {
-        if(empty($params[0])||!empty($params[1]))
-        {
-            $data = array("status" => false, "data" => array());
-            return $data;
-        }elseif((double)$params[0]-(int)$params[0]!=0||!(int)$params[0]>0)
-        {
-            $data = array("status" => false, "data" => array());
-            return $data;
-        }
-        else
-        {
-            $x = (int)$params[0];
-            if ($x % 400 == 0 || $x % 4 == 0 && $x % 100 != 0) {
-                $data = array("status" => true, "data" => array("result" => "Nam " . $x . " la nam nhuan"));
-
-            } else {
-                $data = array("status" => true, "data" => array("result" => "Nam " . $x . " khong phai la nam nhuan"));
-            }
-            return $data;
-        }
+        $date = "22-4-2020"
+        $myDate = new DateTime($date);
+        $myDate->modify("+1 day");
+        echo $myDate->format("l, d, m, Y");
+        
     }
 }
 
